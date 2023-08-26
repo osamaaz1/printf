80c5a01a33991e42fdbbebca67c2b6489d772d0a
Osama <osama.az1.os@gmail.com>
2023-08-26 01:17:43 -0700

    n

--- main.h
+++ main.h
@@ -1,83 +1,9 @@
-#ifndef PRINT_UTILS_H
-#define PRINT_UTILS_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdarg.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <stdarg.h>
 
-#define UNUSED_VAR(x) (void)(x)
-#define BUFFER_SIZE 1024
+int _printf(const char *format, ...);
 
-#define FLAG_MINUS 1
-#define FLAG_PLUS 2
-#define FLAG_ZERO 4
-#define FLAG_HASH 8
-#define FLAG_SPACE 16
-
-#define MOD_LONG 3
-#define MOD_SHORT 1
-
-struct format_handler {
-    char specifier;
-    int (*handler_fn)(va_list, char[], int, int, int, int);
-};
-
-typedef struct format_handler format_handler_t;
-
-int custom_printf(const char *format, ...);
-int handle_format(const char *fmt, int *i,
-    va_list list, char buffer[], int flags, int width, int precision, int size);
-
-/****************** FUNCTION HANDLERS ******************/
-
-int handle_char(va_list args, char buffer[],
-    int flags, int width, int precision, int size);
-int handle_string(va_list args, char buffer[],
-    int flags, int width, int precision, int size);
-int handle_percent(va_list args, char buffer[],
-    int flags, int width, int precision, int size);
-int handle_int(va_list args, char buffer[],
-    int flags, int width, int precision, int size);
-int handle_binary(va_list args, char buffer[],
-    int flags, int width, int precision, int size);
-int handle_unsigned(va_list args, char buffer[],
-    int flags, int width, int precision, int size);
-int handle_octal(va_list args, char buffer[],
-    int flags, int width, int precision, int size);
-int handle_hexadecimal(va_list args, char buffer[],
-    int flags, int width, int precision, int size);
-int handle_hexadecimal_upper(va_list args, char buffer[],
-    int flags, int width, int precision, int size);
-int handle_non_printable(va_list args, char buffer[],
-    int flags, int width, int precision, int size);
-int handle_pointer(va_list args, char buffer[],
-    int flags, int width, int precision, int size);
-int handle_reverse(va_list args, char buffer[],
-    int flags, int width, int precision, int size);
-int handle_rot13(va_list args, char buffer[],
-    int flags, int width, int precision, int size);
-
-int get_format_flags(const char *format, int *i);
-int get_format_width(const char *format, int *i, va_list list);
-int get_format_precision(const char *format, int *i, va_list list);
-int get_format_modifier(const char *format, int *i);
-
-int write_formatted_char(char c, char buffer[],
-    int flags, int width, int precision, int size);
-int write_formatted_number(int is_positive, int ind, char buffer[],
-    int flags, int width, int precision, int size);
-int write_formatted_num(int ind, char bff[], int flags, int width, int precision,
-    int length, char padding_char, char extra_char);
-int write_formatted_pointer(char buffer[], int ind, int length,
-    int width, int flags, char padding_char, char extra_char, int padding_start);
-int write_formatted_unsigned(int is_negative, int ind,
-    char buffer[], int flags, int width, int precision, int size);
-
-int is_printable_char(char c);
-int append_formatted_hex_code(char value, char buffer[], int index);
-int is_digit_char(char c);
-
-long int convert_size_number(long int num, int size);
-long int convert_size_unsigned(unsigned long int num, int size);
-
-#endif /* PRINT_UTILS_H */
+#endif /* MAIN_H */
--- printf.c
+++ printf.c
@@ -1,65 +1,52 @@
-#include "print_utils.h"
-
-void print_output_buffer(char output_buffer[], int *output_buffer_index);
-
+#include "main.h"
 /**
- * custom_printf - Custom printf function
- * @format: Format string.
- * Return: Number of printed characters.
+ * _printf - Custom printf function
+ * @format: Format string
+ *
+ * Return: Number of characters printed
  */
-int custom_printf(const char *format, ...)
+int _printf(const char *format, ...)
 {
-	int i, printed_count = 0, total_printed_chars = 0;
-	int format_flags = 0, format_width = 0, format_precision = 0;
-	int format_modifier = 0, output_buffer_index = 0;
-	va_list arguments_list;
-	char output_buffer[BUFFER_SIZE];
+	int i, count = 0;
+	va_list args;
 
-	if (format == NULL)
-		return (-1);
-	va_start(arguments_list, format);
+	va_start(args, format);
 
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] != '%')
+	if (format[i] == '%')
+	{
+		i++;
+		switch (format[i])
 		{
-			output_buffer[output_buffer_index++] = format[i];
-			if (output_buffer_index == BUFFER_SIZE)
-				print_output_buffer(output_buffer, &output_buffer_index);
-			printed_count++;
+		case 'c':
+		{
+			char c = va_arg(args, int);
+			count += write(1, &c, 1);
+			break;
 		}
-		else
+		case 's':
 		{
-			print_output_buffer(output_buffer, &output_buffer_index);
-			format_flags = get_format_flags(format, &i);
-			format_width = get_format_width(format, &i, arguments_list);
-			format_precision = get_format_precision(format, &i, arguments_list);
-			format_modifier = get_format_modifier(format, &i);
-			++i;
-			printed_count = handle_format(format, &i, arguments_list, output_buffer,
-				format_flags, format_width, format_precision, format_modifier);
-			if (printed_count == -1)
-				return (-1);
-			total_printed_chars += printed_count;
+			char *s = va_arg(args, char *);
+			count += write(1, s, strlen(s));
+			break;
+		}
+		case '%':
+		{
+			count += write(1, "%", 1);
+			break;
+		}
+		default:
+			break;
 		}
 	}
+	else
+	{
+		count += write(1, &format[i], 1);
+	}
+	}
 
-	print_output_buffer(output_buffer, &output_buffer_index);
+	va_end(args);
 
-	va_end(arguments_list);
-
-	return (total_printed_chars);
-}
-
-/**
- * print_output_buffer - Prints the contents of the output buffer if it exists
- * @output_buffer: Array of characters to be printed
- * @output_buffer_index: Index indicating the current length of the buffer
- */
-void print_output_buffer(char output_buffer[], int *output_buffer_index)
-{
-	if (*output_buffer_index > 0)
-		write(1, &output_buffer[0], *output_buffer_index);
-
-	*output_buffer_index = 0;
+	return (count);
 }
