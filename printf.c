930058430c20f9636e6ddf07c6bcdebbcf5d0267
Osama <osama.az1.os@gmail.com>
2023-08-26 01:50:59 -0700

    1

--- printf.c
+++ printf.c
@@ -1,32 +1,45 @@
+#include "main.h"
 /**
- * _printf - This function produces output according to a format.
- * @format: The format string containing conversion specifiers.
- * Return: The number of characters printed (excluding the null byte).
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
  */
-int _printf(const char *format, ...)
+int _printf(const char * const format, ...)
 {
-		va_list args;
-	int count = 0;
+	convert_match m[] = {
+		{"%s", printf_string}, {"%c", printf_char},
+		{"%%", printf_37},
+		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
+		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+	};
+
+	va_list args;
+	int i = 0, j, len = 0;
 
 	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 
-	while (*format)
+Here:
+	while (format[i] != '\0')
 	{
-		if (*format != '%')
-			putchar(*format), count++;
-		else
+		j = 13;
+		while (j >= 0)
 		{
-			format++;
-			if (*format == 'c')
-				putchar(va_arg(args, int));
-				count++;
-			else if (*format == 's')
-				for (char *str = va_arg(args, char *); *str; putchar(*str++), count++);
-			else if (*format == '%')
-				putchar('%'), count++;
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
 		}
-		format++;
+		_putchar(format[i]);
+		len++;
+		i++;
 	}
 	va_end(args);
-	return count;
+	return (len);
 }
