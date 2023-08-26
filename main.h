0eb8951cd6a30967a3f6c6f7f63d3e857ad178e6
Osama <osama.az1.os@gmail.com>
2023-08-26 01:49:37 -0700

    1

--- main.h
+++ main.h
@@ -1,17 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <stdarg.h>
-
-
-typedef struct format
-{
-	char *id;
-	int (*f)();
-} convert_match;
-
-int _putchar(char c);
+#include <stdio.h>
 int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
+
--- printf.c
+++ printf.c
@@ -1,49 +1,32 @@
-#include "main.h"
-
 /**
- * _printf - This function selects the correct conversion function to print
- *           based on the provided format.
+ * _printf - This function produces output according to a format.
  * @format: The format string containing conversion specifiers.
- * Return: The total number of characters printed.
+ * Return: The number of characters printed (excluding the null byte).
  */
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
-	convert_match conversion_funcs[] = {
-	{"%s", printf_string}, {"%c", printf_char},
-	{"%%", printf_percent},
-	{"%i", printf_int}, {"%d", printf_decimal}, {"%r", printf_reverse},
-	{"%R", printf_rot13}, {"%b", printf_binary}, {"%u", printf_unsigned},
-	{"%o", printf_octal}, {"%x", printf_hex}, {"%X", printf_HEX},
-	{"%S", printf_exclusive_string}, {"%p", printf_pointer}
-	};
-
-	va_list args;
-	int i = 0, j, total_chars_printed = 0;
+		va_list args;
+	int count = 0;
 
 	va_start(args, format);
-	while (format[i] != '\0')
+
+	while (*format)
 	{
-		j = 13;
-		while (j >= 0)
+		if (*format != '%')
+			putchar(*format), count++;
+		else
 		{
-			if (conversion_funcs[j].specifier[0] == format[i] &&
-			conversion_funcs[j].specifier[1] == format[i + 1])
-			{
-			total_chars_printed += conversion_funcs[j].function(args);
-			i = i + 2;
-			goto Continue;
-			}
-			j--;
+			format++;
+			if (*format == 'c')
+				putchar(va_arg(args, int));
+				count++;
+			else if (*format == 's')
+				for (char *str = va_arg(args, char *); *str; putchar(*str++), count++);
+			else if (*format == '%')
+				putchar('%'), count++;
 		}
-
-		_putchar(format[i]);
-		total_chars_printed++;
-		i++;
-
-Continue:
-	continue;
+		format++;
 	}
-
 	va_end(args);
-	return (total_chars_printed);
+	return count;
 }
