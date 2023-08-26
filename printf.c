d948b91d4264e1bad9eb75333f0b09c72994c9fb
Osama <osama.az1.os@gmail.com>
2023-08-26 01:23:59 -0700

    d

--- printf.c
+++ printf.c
@@ -11,42 +11,39 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
-
 	for (i = 0; format[i] != '\0'; i++)
 	{
-	if (format[i] == '%')
-	{
-		i++;
-		switch (format[i])
+		if (format[i] == '%')
 		{
-		case 'c':
-		{
-			char c = va_arg(args, int);
-			count += write(1, &c, 1);
-			break;
-		}
-		case 's':
-		{
-			char *s = va_arg(args, char *);
-			count += write(1, s, strlen(s));
-			break;
-		}
-		case '%':
-		{
-			count += write(1, "%", 1);
-			break;
-		}
-		default:
-			break;
-		}
-	}
-	else
-	{
-		count += write(1, &format[i], 1);
-	}
-	}
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+				{
+					char c = va_arg(args, int);
 
+					count += write(1, &c, 1);
+					break;
+				}
+				case 's':
+				{
+					char *s = va_arg(args, char *);
+
+					count += write(1, s, strlen(s));
+					break;
+				}
+				case '%':
+				{
+					count += write(1, "%", 1);
+					break;
+				}
+				default:
+				break;
+			}
+		}
+		else
+			count += write(1, &format[i], 1);
+	}
 	va_end(args);
-
 	return (count);
 }
