d076350c0085665647628bbd1dcbaa3eb06194fe
Osama <osama.az1.os@gmail.com>
2023-08-26 02:01:16 -0700

    1

--- main.h
+++ main.h
@@ -10,8 +10,11 @@ typedef struct format
 } convert_match;
 
 
-int _printf(const char *format, ...);
+int printf_37(void);
+int printf_char(va_list val);
+int printf_string(va_list val);
 int _putchar(char c);
+int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
 
