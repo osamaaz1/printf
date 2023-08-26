4da867b671261f63a6c3d2dc3a78e7965f1abbaa
Osama <osama.az1.os@gmail.com>
2023-08-26 02:02:50 -0700

    1

--- main.h
+++ main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
 typedef struct format
 {
 	char *id;
